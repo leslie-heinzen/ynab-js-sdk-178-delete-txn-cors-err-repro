3fb35f1e95fbfddf61c197651bbdb58b71a0a559
Leslie Heinzen <leslie.heinzen@gmail.com>
2024-11-04 11:10:15 -0500

    Repro ynab api DELETE http method CORS err

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import * as ynab from "ynab";
+
+const accessToken = "<access-token>";
+const ynabAPI = new ynab.API(accessToken);
+const budgetId = "<budget-id>";
+const accountId = "<account-id>";
+const categoryId = "<category-id>";
 
 function App() {
+  useEffect(() => {
+    async function createAndDeleteTransaction() {
+      try {
+        // create transaction
+        const txn = await ynabAPI.transactions.createTransaction(budgetId, {
+          transaction: {
+            account_id: accountId,
+            category_id: categoryId,
+            payee_id: null,
+            cleared: "cleared",
+            approved: true,
+            date: ynab.utils.getCurrentDateInISOFormat(),
+            amount: -23430,
+            memo: "Dry Cleaning",
+          },
+        });
+        // wait 1 second
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        // delete transaction
+        await ynabAPI.transactions.deleteTransaction(
+          budgetId,
+          txn.data.transaction.id
+        );
+      } catch (err) {
+        const error = err.error;
+        if (!error) {
+          console.error(err);
+          return;
+        }
+
+        console.error(
+          `ERROR: id=${error.id}; name=${error.name}; detail: ${error.detail}`
+        );
+      }
+    }
+
+    createAndDeleteTransaction();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
